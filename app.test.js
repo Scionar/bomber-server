a08563a3dd8dc879b1a50c4030e6eb92db0ff07d
Joona Viertola <contact@joonaviertola.com>
2018-02-18 14:28:29 +0200

    Added first test and added that feature.

---
+++ app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('Test root path', () => {
+  test('It should response to GET request', done => {
+    request(app)
+      .get('/')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
