02d0b80eae9fd333451dedb55d809a3a7f961ddc
Joona Viertola <contact@joonaviertola.com>
2018-03-12 18:27:40 +0200

    Added last move action creators to movePlayer event. Also added store dispatches.

--- events/movePlayer.js
+++ events/movePlayer.js
@@ -1,6 +1,9 @@
 const actions = require('../actions');
+const store = require('../store');
 
 module.exports = (id, direction) => {
-  if (direction === 'up') actions.movePlayerUp(id);
-  if (direction === 'down') actions.movePlayerDown(id);
+  if (direction === 'up') store.dispatch(actions.movePlayerUp(id));
+  if (direction === 'down') store.dispatch(actions.movePlayerDown(id));
+  if (direction === 'left') store.dispatch(actions.movePlayerLeft(id));
+  if (direction === 'right') store.dispatch(actions.movePlayerRight(id));
 };
--- events/movePlayer.test.js
+++ events/movePlayer.test.js
@@ -1,24 +1,39 @@
 jest.mock('../actions');
+jest.mock('../store');
 const movePlayer = require('./movePlayer');
 const actions = require('../actions');
 
 describe('Move player up event', () => {
   test('Calls movePlayerUp action creator with same id', () => {
-    const id = 123;
+    const id = 1;
     movePlayer(id, 'up');
     expect(actions.movePlayerUp).toHaveBeenCalledWith(id);
     actions.movePlayerUp.mockClear();
   });
 
   test('Calls movePlayerDown action creator with same id', () => {
-    const id = 123;
+    const id = 2;
     movePlayer(id, 'down');
     expect(actions.movePlayerDown).toHaveBeenCalledWith(id);
-    actions.movePlayerUp.mockClear();
+    actions.movePlayerDown.mockClear();
+  });
+
+  test('Calls movePlayerLeft action creator with same id', () => {
+    const id = 3;
+    movePlayer(id, 'left');
+    expect(actions.movePlayerLeft).toHaveBeenCalledWith(id);
+    actions.movePlayerLeft.mockClear();
+  });
+
+  test('Calls movePlayerRight action creator with same id', () => {
+    const id = 3;
+    movePlayer(id, 'right');
+    expect(actions.movePlayerRight).toHaveBeenCalledWith(id);
+    actions.movePlayerRight.mockClear();
   });
 
   test('Does not call movePlayerUp action creator with invalid direction', () => {
-    const id = 123;
+    const id = 4;
     movePlayer(id, 'argh');
     expect(actions.movePlayerUp).not.toHaveBeenCalled();
     actions.movePlayerUp.mockClear();
